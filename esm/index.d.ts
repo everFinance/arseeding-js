32bb8580809e12630a870317eb3d4829d53caaeb
xaber <xaber.twt@gmail.com>
2022-07-05 18:12:38 +0800

    chore: remove build esm

--- esm/index.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="node" />
-export declare const sendAndPay: (arseedingUrl: string, windowEthereum: never, data: Buffer, tokenSymbol: string) => Promise<void>;
-//# sourceMappingURL=index.d.ts.map
