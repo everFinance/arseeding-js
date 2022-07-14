1a62569bb99b9932a2aa460c0235a6b473bfa3d9
sandy <18382255942@163.com>
2022-07-14 18:37:29 +0800

    feat(): add example

--- example/uploadFile.ts
+++ example/uploadFile.ts
@@ -1,12 +1,12 @@
 import { CreateAndSubmitItem } from '../src/submitOrder'
 import EthereumSigner from 'arseeding-arbundles/src/signing/chains/ethereumSigner'
+// import ArweaveSigner from "arseeding-arbundles/src/signing/chains/ArweaveSigner";
+// import {readFileSync} from "fs";
+// import path from "path";
+import { Signer } from 'arseeding-arbundles/src/signing'
 
 // ts-node ./example/uploadFile.ts
-async function uploadFile () {
-    // this key is used to bundle data signer
-    const privateKey = ''
-    const eccSigner = new EthereumSigner(privateKey)
-
+async function uploadFile (signer: Signer) {
     const ops = {
         tags: [
             { name: 'key01', value: 'val01' },
@@ -16,9 +16,18 @@ async function uploadFile () {
     const arseedingUrl = 'https://arseed.web3infura.io'
     const tokenSymbol = 'VRT'
     const data = Buffer.from('aa bb cc')
-    const order = await CreateAndSubmitItem(eccSigner, data, ops, arseedingUrl, tokenSymbol)
+    const order = await CreateAndSubmitItem(signer, data, ops, arseedingUrl, tokenSymbol)
     console.log('order', order)
 }
 
-uploadFile()
+// ecc signer
+const privateKey = ''
+const eccSigner = new EthereumSigner(privateKey)
+uploadFile(eccSigner)
 
+// // rsa signer
+// const wallet = JSON.parse(
+//     readFileSync(path.join(__dirname, "test_key.json")).toString(),
+// );
+// const rsaSigner = new ArweaveSigner(wallet)
+// uploadFile(rsaSigner)
